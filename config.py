5e4732f2c77e7df8a7602bcbf950a8963e1f2274
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-02-25 04:11:43 +0300

    returned folders for web (static, temps)

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ class Config():
 	def __init__(self):
 		with open(CONFIG_NAMEFILE, 'r') as f:
 			self.data = json.load(f)
-
+	def to_dict(self) -> dict:
+		return self.data
+	def get(self, key:str) -> str:
+		return f"{self.to_dict[key]}"
