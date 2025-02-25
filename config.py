c594e1cc8aead4488763b06e784d65c1b0282bad
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-02-25 04:58:53 +0300

    refactoring code

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import json
-CONFIG_NAMEFILE = "config.json"
 
 class Config():
-	def __init__(self):
-		with open(CONFIG_NAMEFILE, 'r') as f:
+	def __init__(self, namefile:str) -> None:
+		with open(namefile, 'r') as f:
 			self.data = json.load(f)
 	def to_dict(self) -> dict:
 		return self.data
 	def get(self, key:str) -> str:
-		return f"{self.to_dict[key]}"
+		return f"{self.to_dict()[key]}"
